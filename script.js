d8065a56b5d542c41f5113b267af0d76b4321a78
sirvintechdev <sirvinpinoy@gmail.com>
2026-06-25 23:17:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function showMessage() {
-
     document.getElementById("message").innerHTML =
-        "Welcome to Pinoy Eye Witness in Europe! We bring Filipino stories, travel experiences, community news, and inspiring events from across Europe.";
-
+        "Welcome! Explore Filipino stories across Europe.";
 }
