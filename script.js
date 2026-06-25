74513259be40f2b7e4dc909a9ab05cad04fcbff0
sirvintechdev <sirvinpinoy@gmail.com>
2026-06-25 20:55:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+
+    document.getElementById("message").innerHTML =
+        "Welcome to Pinoy Eye Witness in Europe! We bring Filipino stories, travel experiences, community news, and inspiring events from across Europe.";
+
+}
